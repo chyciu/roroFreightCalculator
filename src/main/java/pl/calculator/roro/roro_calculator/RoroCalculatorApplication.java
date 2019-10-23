e146e4644f0620147050d7f36c9d319386b38efa
chyciu <jarek.chwyc@yahoo.com>
2019-10-23 20:45:49 +0200

    first commit

---
+++ src/main/java/pl/calculator/roro/roro_calculator/RoroCalculatorApplication.java
@@ -0,0 +1,13 @@
+package pl.calculator.roro.roro_calculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoroCalculatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RoroCalculatorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/calculator/roro/roro_calculator/RoroCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.calculator.roro.roro_calculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoroCalculatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
